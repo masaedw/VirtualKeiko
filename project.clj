d85c65ba65324d7d7c29f52e6cacef2354a08133
Masayuki Muto <muto@timedia.co.jp>
2012-09-07 16:00:21 +0900

    lein new compojure virtualkeiko

---
+++ project.clj
@@ -0,0 +1,9 @@
+(defproject virtualkeiko "0.1.0-SNAPSHOT"
+  :description "FIXME: write description"
+  :url "http://example.com/FIXME"
+  :dependencies [[org.clojure/clojure "1.4.0"]
+                 [compojure "1.1.1"]]
+  :plugins [[lein-ring "0.7.3"]]
+  :ring {:handler virtualkeiko.handler/app}
+  :profiles
+  {:dev {:dependencies [[ring-mock "0.1.3"]]}})
