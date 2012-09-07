075349f150cf1b2349099b84389ee6ea7387cabd
Masayuki Muto <muto@timedia.co.jp>
2012-09-07 19:23:30 +0900

    add lein version

--- project.clj
+++ project.clj
@@ -1,4 +1,5 @@
 (defproject virtualkeiko "0.1.0-SNAPSHOT"
+  :min-lein-version "2.0.0"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :dependencies [[org.clojure/clojure "1.4.0"]
