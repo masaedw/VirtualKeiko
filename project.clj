889f00717301b446806ff6479f75e28bc167eeef
Masayuki Muto <muto@timedia.co.jp>
2012-09-07 16:04:20 +0900

    use congomongo

--- project.clj
+++ project.clj
@@ -2,7 +2,8 @@
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :dependencies [[org.clojure/clojure "1.4.0"]
-                 [compojure "1.1.3"]]
+                 [compojure "1.1.3"]
+                 [congomongo "0.1.9"]]
   :plugins [[lein-ring "0.7.5"]]
   :ring {:handler virtualkeiko.handler/app}
   :profiles
