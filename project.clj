ac34761534c558997debf3204020f46231689e86
Masayuki Muto <muto@timedia.co.jp>
2012-09-07 18:34:07 +0900

    use hiccup and lein-swank

--- project.clj
+++ project.clj
@@ -3,8 +3,10 @@
   :url "http://example.com/FIXME"
   :dependencies [[org.clojure/clojure "1.4.0"]
                  [compojure "1.1.3"]
-                 [congomongo "0.1.9"]]
-  :plugins [[lein-ring "0.7.5"]]
+                 [congomongo "0.1.9"]
+                 [hiccup "1.0.1"]]
+  :plugins [[lein-ring "0.7.5"]
+            [lein-swank "1.4.2"]]
   :ring {:handler virtualkeiko.handler/app}
   :profiles
   {:dev {:dependencies [[ring-mock "0.1.3"]]}})
