3587858a481cf6d2ba8029bdf8e4bdb6c9c3fb5f
masaedw <masa.edw@gmail.com>
2013-08-24 14:41:22 +0900

    set url

--- project.clj
+++ project.clj
@@ -1,7 +1,7 @@
 (defproject virtualkeiko "0.1.0-SNAPSHOT"
   :min-lein-version "2.0.0"
   :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :url "http://virtualkeiko.herokuapp.com"
   :dependencies [[org.clojure/clojure "1.4.0"]
                  [compojure "1.1.5"]
                  [congomongo "0.4.1"]
